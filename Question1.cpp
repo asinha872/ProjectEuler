44307492620a0fcac1b2634cb2aa25d52b2faf36
asinha872 <asinha872@gmail.com>
2015-11-28 12:12:27 +0530

    Create Question1.cpp

---
+++ Question1.cpp
@@ -0,0 +1,21 @@
+/*
+https://projecteuler.net/problem=1
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a3,a5,a15;
+	int n=1000;
+	a3=(n-1)/3;
+	a5=(n-1)/5;
+	a15=(n-1)/15;
+	a3=3*a3*(a3+1)/2;
+	a5=5*a5*(a5+1)/2;
+	a15=15*a15*(a15+1)/2;
+	
+	int ans= a3+a5-a15;
+	
+	cout<<ans<<endl;
+}
